0b1c896ab532cfad22803e6e5d9b7884092f6ea7
miguel <corbos1995@gmail.com>
2023-06-12 12:27:18 -0400

    fix db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-import cobroJSON from "./data_2/Cobro.json";
-import perfilJSON from "./data_2/Perfil.json";
-import grupoJSON from "./data_2/Grupo.json";
-import pagoJSON from "./data_2/Pago.json";
-import data from "./data_2/demo-data.json";
+import cobroJSON from "./data_3/Cobro.json";
+import perfilJSON from "./data_3/Perfil.json";
+import grupoJSON from "./data_3/Grupo.json";
+import pagoJSON from "./data_3/Pago.json";
+import data from "./data_3/demo-data.json";
 const prisma = new PrismaClient();
 
 async function main() {
   await createPerfiles();
   await createGroups();
-  await createCharge();
+   await createCharge();
   await createPayments();
 }
 
 async function createPerfiles() {
   await prisma.perfil.deleteMany();
+
   for (let data of perfilJSON) {
     await prisma.perfil.create({
       data,
@@ -24,9 +25,11 @@ async function createPerfiles() {
 
 async function createGroups() {
   await prisma.grupo.deleteMany();
+  
   for (let data of grupoJSON) {
+   
     await prisma.grupo.create({
-      data,
+    data,
     });
   }
 }
@@ -34,12 +37,12 @@ async function createGroups() {
 async function createCharge() {
   await prisma.cobro.deleteMany();
   for (let data of cobroJSON) {
+  
     const fecha = new Date(data.fecha);
 
     await prisma.cobro.create({
-      data: {
-        ...data,
-        fecha,
+      data:{
+        ...data,fecha, 
       },
     });
   }
@@ -48,10 +51,12 @@ async function createCharge() {
 async function createPayments() {
   await prisma.pago.deleteMany();
   for (let data of pagoJSON) {
+
     const fecha = new Date(data.fecha);
+    
     await prisma.pago.create({
-      data: {
-        ...data,
+      data: {        
+       ...data,
         fecha,
       },
     });
