fd29806e59a1edce098bba67e8d70cc07f499bcc
Miguel Corbos <corbos1995@gmail.com>
2023-06-15 14:34:49 -0400

    confirm

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,100 +1,97 @@
-import { PrismaClient } from "@prisma/client";
-import cobroJSON from "./data_3/Cobro.json";
-import perfilJSON from "./data_3/Perfil.json";
-import grupoJSON from "./data_3/Grupo.json";
-import pagoJSON from "./data_3/Pago.json";
-import data from "./data_3/demo-data.json";
+import { PrismaClient } from '@prisma/client';
+import cobroJSON from './data_3/Cobro.json';
+import perfilJSON from './data_3/Perfil.json';
+import grupoJSON from './data_3/Grupo.json';
+import pagoJSON from './data_3/Pago.json';
+import data from './data_3/demo-data.json';
 const prisma = new PrismaClient();
 
 async function main() {
-  await createPerfiles();
-  await createGroups();
-  await createCharge();
-  await createPayments();
+   await prisma.pago.deleteMany();
+   await prisma.cobro.deleteMany();
+   await prisma.grupo.deleteMany();
+   await prisma.perfil.deleteMany();
+
+   await createPerfiles();
+   await createGroups();
+   await createCharge();
+   await createPayments();
 }
 
 async function createPerfiles() {
-  await prisma.perfil.deleteMany();
+   for (let data of perfilJSON) {
+      const { cedula, id, subName, userId } = data;
 
-  for (let data of perfilJSON) {
-    const { cedula, id, subName, userId } = data;
-
-    await prisma.perfil.create({
-      data: {
-        id,
-        cedula,
-        nombre: subName,
-        userId,
-      },
-    });
-  }
+      await prisma.perfil.create({
+         data: {
+            id,
+            cedula,
+            nombre: subName,
+            userId,
+         },
+      });
+   }
 }
 
 async function createGroups() {
-  await prisma.grupo.deleteMany();
-
-  for (let data of grupoJSON) {
-    await prisma.grupo.create({
-      data,
-    });
-  }
+   for (let data of grupoJSON) {
+      await prisma.grupo.create({
+         data,
+      });
+   }
 }
 
 async function createCharge() {
-  await prisma.cobro.deleteMany();
-  for (let data of cobroJSON) {
-    const fecha = new Date(data.fecha);
+   for (let data of cobroJSON) {
+      const fecha = new Date(data.fecha);
 
-    await prisma.cobro.create({
-      data: {
-        ...data,
-        fecha,
-      },
-    });
-  }
+      await prisma.cobro.create({
+         data: {
+            ...data,
+            fecha,
+         },
+      });
+   }
 }
 
 async function createPayments() {
-  await prisma.pago.deleteMany();
-  for (let data of pagoJSON) {
-    const {
-      captureImg,
-      cobroId,
-      grupoId,
-      id,
-      monto,
-      perfilId,
-      suscritoId,
-      referencia,
-    } = data;
+   for (let data of pagoJSON) {
+      const {
+         captureImg,
+         cobroId,
+         grupoId,
+         id,
+         monto,
+         perfilId,
+         suscritoId,
+         referencia,
+      } = data;
 
-    const fecha = new Date(data.fecha);
+      const fecha = new Date(data.fecha);
 
-    await prisma.pago.create({
-      data: {
-        captureImg,
-        estado: 1,
-        refAdmin: "",
-        Observacion: "123",
-        cobroId,
-        perfilId,
-        suscritoId,
-        referencia: referencia.toString(),
-        monto,
-        fecha,
-        grupoId,
-        id,
-      },
-    });
-  }
+      await prisma.pago.create({
+         data: {
+            captureImg,
+            estado: 1,
+            cobroId,
+            perfilId,
+            suscritoId,
+            referencia: referencia.toString(),
+            monto,
+            fecha,
+            grupoId,
+            id,
+         },
+      });
+   }
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+   .then(async () => {
+      await prisma.$disconnect();
+   })
+   .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+   });
--- src/app/paymonitor/cobros/list/filtros.tsx
+++ src/app/paymonitor/cobros/list/filtros.tsx
@@ -1,33 +1,42 @@
-"use client";
+'use client';
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import {
-  Flex,
-  TextInput,
-  DateRangePicker,
-  DateRangePickerValue,
-} from "@tremor/react";
-import {} from "react-hook-form";
-import React from "react";
+   Flex,
+   TextInput,
+   DateRangePicker,
+   DateRangePickerValue,
+   Button,
+} from '@tremor/react';
+import { useFormContext } from 'react-hook-form';
+import React from 'react';
 
 function Filtros() {
-  const [value, setValue] = React.useState<DateRangePickerValue>({
-    from: new Date(2023, 1, 1),
-    to: new Date(),
-  });
+   const { register } = useFormContext();
 
-  return (
-    <>
-      <Flex className="mt-1 gap-1 flex-wrap md:flex-nowrap">
-        <TextInput
-          className="w-full"
-          placeholder="Buscar por nombre"
-          icon={MagnifyingGlassIcon}
-        />
-        <DateRangePicker className="mx-auto w-full" />
-      </Flex>
-    </>
-  );
+   const [value, setValue] = React.useState<DateRangePickerValue>({
+      from: new Date(2023, 1, 1),
+      to: new Date(),
+   });
+
+   return (
+      <>
+         <Flex className='mt-1 gap-1 flex-wrap md:flex-nowrap'>
+            <TextInput
+               {...register('descripcion')}
+               className='w-full'
+               placeholder='Buscar por nombre'
+               icon={MagnifyingGlassIcon}
+            />
+            <DateRangePicker className='mx-auto w-full' />
+            <Button
+               type='submit'
+               size='xs'>
+               Buscar
+            </Button>
+         </Flex>
+      </>
+   );
 }
 
 export default Filtros;
