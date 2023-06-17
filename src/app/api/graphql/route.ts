43f89f195878be9d99b3460b018e6e234291d629
Miguel Corbos <corbos1995@gmail.com>
2023-06-17 00:56:55 -0400

    co

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,18 +1,30 @@
 // Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
-import { createYoga } from 'graphql-yoga';
-import { schema } from '@app/graphql/schema';
-import { Session, getServerSession } from 'next-auth';
-import { authOptions } from '@app/app/api/auth/[...nextauth]/route';
+import { createYoga } from "graphql-yoga";
+import { schema } from "@app/graphql/schema";
+import { Session, getServerSession } from "next-auth";
+import { authOptions } from "@app/app/api/auth/[...nextauth]/route";
 
 const { handleRequest } = createYoga<{ session: Session }>({
-   schema,
-   async context() {
-      return { session: await getServerSession(authOptions) };
-   },
-   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
-   graphqlEndpoint: '/api/graphql',
-   // Yoga needs to know how to create a valid Next response
-   fetchAPI: { Response },
+  cors: {
+    exposedHeaders: ["http://localhost:5173"],
+  },
+  schema,
+  async context() {
+    return { session: await getServerSession(authOptions) };
+  },
+  // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
+  graphqlEndpoint: "/api/graphql",
+  // Yoga needs to know how to create a valid Next response
+  fetchAPI: {
+    Response: {},
+
+    Headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  },
+  healthCheckEndpoint: "/live",
 });
 
 export { handleRequest as GET, handleRequest as POST };
