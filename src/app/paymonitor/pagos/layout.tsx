48951f6c63af9bb06a9357216e86fadfc733f903
Miguel Corbos <corbos1995@gmail.com>
2023-07-17 08:57:59 -0400

    Fixed 2

--- src/app/paymonitor/pagos/layout.tsx
+++ src/app/paymonitor/pagos/layout.tsx
@@ -2,21 +2,21 @@ import { Flex, Text, Title } from '@tremor/react';
 import PageNavbar from './pageNavbar';
 
 const navigation = [
-   { name: 'Todos', href: '/paymonitor/pagos' },
-   { name: 'Estadisticas', href: '/paymonitor/pagos/estadisticas' },
+  { name: 'Todos', href: '/paymonitor/pagos' },
+  { name: 'Estadísticas', href: '/paymonitor/pagos/estadisticas' },
 ];
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-   return (
-      <>
-         <Flex>
-            <Title>Mis Pagos</Title>
-         </Flex>
-         <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsa eligendi voluptatibus.
-         </Text>
-         <PageNavbar navlinks={navigation} />
-         {children}
-      </>
-   );
+  return (
+    <>
+      <Flex>
+        <Title>Mis Pagos</Title>
+      </Flex>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsa eligendi voluptatibus.
+      </Text>
+      <PageNavbar navlinks={navigation} />
+      {children}
+    </>
+  );
 }
--- src/libs/apollo-wrapper.tsx
+++ src/libs/apollo-wrapper.tsx
@@ -1,89 +1,81 @@
 'use client';
 
-import { ApolloLink, HttpLink, SuspenseCache } from '@apollo/client';
+import { ApolloLink, HttpLink } from '@apollo/client';
 
 import {
-   NextSSRApolloClient,
-   ApolloNextAppProvider,
-   NextSSRInMemoryCache,
-   SSRMultipartLink,
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
 } from '@apollo/experimental-nextjs-app-support/ssr';
 import clientCookies from 'js-cookie';
 import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 import { setVerbosity } from 'ts-invariant';
 
 if (process.env.NODE_ENV === 'development') {
-   setVerbosity('debug');
-   loadDevMessages();
-   loadErrorMessages();
-}
-
-function makeSuspenseCache() {
-   return new SuspenseCache();
+  setVerbosity('debug');
+  loadDevMessages();
+  loadErrorMessages();
 }
 
 export function ApolloWrapper({
-   children,
-   delay: delayProp,
-   token: tokenProp,
-   csrf: csrfProp,
+  children,
+  delay: delayProp,
+  token: tokenProp,
+  csrf: csrfProp,
 }: React.PropsWithChildren<{ delay: number; token: string; csrf: string }>) {
-   return (
-      <ApolloNextAppProvider makeClient={makeClient} makeSuspenseCache={makeSuspenseCache}>
-         {children}
-      </ApolloNextAppProvider>
-   );
+  return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
 
-   function makeClient() {
-      const httpLink = new HttpLink({
-         uri: `${process.env.NEXT_PUBLIC_URI ?? 'http://localhost:3000/api/graphql'}`,
-         fetchOptions: { cache: 'no-store' },
+  function makeClient() {
+    const httpLink = new HttpLink({
+      uri: `${process.env.NEXT_PUBLIC_URI ?? 'http://localhost:3000/api/graphql'}`,
+      fetchOptions: { cache: 'no-store' },
+    });
+
+    const delayLink = new ApolloLink((operation, forward) => {
+      const delay =
+        typeof window === 'undefined'
+          ? delayProp
+          : clientCookies.get('apollo-x-custom-delay') ?? delayProp;
+
+      const authSessionToken =
+        typeof window === 'undefined'
+          ? tokenProp
+          : clientCookies.get('next-auth.session-token') ?? tokenProp;
+
+      const csrfToken =
+        typeof window === 'undefined'
+          ? csrfProp
+          : clientCookies.get('next-auth.csrf-token') ?? csrfProp;
+
+      operation.setContext(({ headers = {} }) => {
+        return {
+          headers: {
+            ...headers,
+            'x-custom-delay': delay,
+            cookie: `next-auth.csrf-token=${csrfToken} ; next-auth.session-token=${authSessionToken}`,
+          },
+        };
       });
+      return forward(operation);
+    });
 
-      const delayLink = new ApolloLink((operation, forward) => {
-         const delay =
-            typeof window === 'undefined'
-               ? delayProp
-               : clientCookies.get('apollo-x-custom-delay') ?? delayProp;
-
-         const authSessionToken =
-            typeof window === 'undefined'
-               ? tokenProp
-               : clientCookies.get('next-auth.session-token') ?? tokenProp;
-
-         const csrfToken =
-            typeof window === 'undefined'
-               ? csrfProp
-               : clientCookies.get('next-auth.csrf-token') ?? csrfProp;
-
-         operation.setContext(({ headers = {} }) => {
-            return {
-               headers: {
-                  ...headers,
-                  'x-custom-delay': delay,
-                  cookie: `next-auth.csrf-token=${csrfToken} ; next-auth.session-token=${authSessionToken}`,
-               },
-            };
-         });
-         return forward(operation);
-      });
-
-      return new NextSSRApolloClient({
-         cache: new NextSSRInMemoryCache(),
-         link:
-            typeof window === 'undefined'
-               ? ApolloLink.from([
-                    // in a SSR environment, if you use multipart features like
-                    // @defer, you need to decide how to handle these.
-                    // This strips all interfaces with a `@defer` directive from your queries.
-                    new SSRMultipartLink({
-                       stripDefer: true,
-                       cutoffDelay: 100,
-                    }),
-                    delayLink,
-                    httpLink,
-                 ])
-               : ApolloLink.from([delayLink, httpLink]),
-      });
-   }
+    return new NextSSRApolloClient({
+      cache: new NextSSRInMemoryCache(),
+      link:
+        typeof window === 'undefined'
+          ? ApolloLink.from([
+              // in a SSR environment, if you use multipart features like
+              // @defer, you need to decide how to handle these.
+              // This strips all interfaces with a `@defer` directive from your queries.
+              new SSRMultipartLink({
+                stripDefer: true,
+                cutoffDelay: 100,
+              }),
+              delayLink,
+              httpLink,
+            ])
+          : ApolloLink.from([delayLink, httpLink]),
+    });
+  }
 }
