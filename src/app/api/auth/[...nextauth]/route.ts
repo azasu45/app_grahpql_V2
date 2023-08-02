acb62be4d4b0a273984368b9f800951c39b22584
Miguel Corbos <corbos1995@gmail.com>
2023-08-01 22:22:52 -0400

    Obc

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,47 +7,50 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { env } from '@app/env.mjs';
 
 declare module 'next-auth' {
-   interface Session extends DefaultSession {
-      user: {
-         id: string;
-         token: string;
-         complete: boolean;
-         // ...other properties
-      } & DefaultSession['user'];
-   }
-
-   interface User {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      token: string;
       complete: boolean;
       // ...other properties
-      token: string;
-   }
+    } & DefaultSession['user'];
+  }
+
+  interface User {
+    complete: boolean;
+    // ...other properties
+    token: string;
+  }
 }
 
 export const authOptions: NextAuthOptions = {
-   session: {
-      strategy: 'database',
-   },
-   adapter: PrismaAdapter(prisma),
-   callbacks: {
-      session({ session, user }) {
-         if (session.user) {
-            session.user.id = user.id;
-            session.user.image = user.image;
-            session.user.name = user.name;
-            session.user.complete = user.complete;
-         }
-         return session;
-      },
-      redirect:({url,baseUrl})=>{
-         return baseUrl
-      },
-   },
-   providers: [
-      GoogleProvider({
-         clientId: env.GOOGLE_CLIENT_ID ?? '',
-         clientSecret: env.GOOGLE_CLIENT_SECRET ?? '',
-      }),
-   ],
+  session: {
+    strategy: 'database',
+  },
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.image = user.image;
+        session.user.name = user.name;
+        session.user.complete = user.complete;
+      }
+      return session;
+    },
+    redirect: ({ url, baseUrl }) => {
+      console.log(url, 'url');
+      console.log(baseUrl, 'base Url');
+
+      return baseUrl;
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
 };
 
 const handler = NextAuth(authOptions);
